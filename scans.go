c8796eb29fcea5f0ed6a828f01c3321a20ad0d55
С5330489 <serhii.holubiev@sap.com>
2024-01-11 21:52:57 +0000

    fix

--- scans.go
+++ scans.go
@@ -115,18 +115,23 @@ func (c Cx1Client) GetScanConfigurationByID(projectID, scanID string) ([]Configu
 	return scanConfigurations, err
 }
 
-func (c Cx1Client) GetScanSummaryCounter() ([]ScanSummaryCount, error) {
+func (c Cx1Client) GetScanSummaryCounter() (ScanSummaryCount, error) {
 	c.logger.Debug("Getting scan status counters")
-	var scanSummaryCount []ScanSummaryCount
+
+	var scanSummaryCount ScanSummaryCount
 
 	data, err := c.sendRequest(http.MethodGet, "/scans/summary", nil, nil)
-
 	if err != nil {
 		c.logger.Trace("Failed to get scan status counters")
 		return scanSummaryCount, err
 	}
+
 	err = json.Unmarshal([]byte(data), &scanSummaryCount)
-	return scanSummaryCount, err
+	if err != nil {
+		return scanSummaryCount, err
+	}
+
+	return scanSummaryCount, nil
 }
 
 func (c Cx1Client) GetScanSummary(scanID string) (ScanSummary, error) {
