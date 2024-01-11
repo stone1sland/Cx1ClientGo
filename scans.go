7b8ffac4e7151f90a87615b5da57e4e7ae8dfd93
С5330489 <serhii.holubiev@sap.com>
2024-01-11 22:23:20 +0000

    debug

--- scans.go
+++ scans.go
@@ -125,7 +125,7 @@ func (c Cx1Client) GetScanSummaryCounter() ([]ScanSummaryCount, error) {
 		c.logger.Trace("Failed to get scan status counters")
 		return scanSummaryCount, err
 	}
-
+	fmt.Printf(string(data))
 	err = json.Unmarshal([]byte(data), &scanSummaryCount)
 	if err != nil {
 		return scanSummaryCount, err
